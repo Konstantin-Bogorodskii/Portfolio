c94f693d6bc2121125340551c0299ebef7d5a05d
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-07-28 18:03:21 +0300

    added animation

--- src/js/components/header-name.js
+++ src/js/components/header-name.js
@@ -1,14 +1,33 @@
 "use strict";
-function fancyText(strSplit, strText) {}
 
 const heroContainer = document.querySelector(".hero__container");
 heroContainer.addEventListener("click", (event) => {
   const target = event.target;
-  if (target.classList.contains("fancy")) {
-    const strText = target.textContent;
-    const strSplit = strText.split("");
-    for (let i = 0; i < strSplit.length; i++) {
-      target.innerHTML += "<span>" + strSplit[i] + "</span>";
+  const strText = target.textContent;
+  const strSplit = strText.split("");
+  if (target.classList.contains("fancy") || target == strText) {
+    target.textContent = "";
+    console.log(target);
+    strSplit.forEach((item) => {
+      target.innerHTML += "<span>" + item + "</span>";
+    });
+    let char = 0;
+    let timer = setInterval(onTick, 50);
+
+    function onTick() {
+      const span = target.querySelectorAll("span")[char];
+      span.classList.add("fancy--active");
+
+      char++;
+      if (char === strSplit.length) {
+        complete();
+        return;
+      }
+    }
+
+    function complete() {
+      clearInterval(timer);
+      timer = null;
     }
   }
 });
