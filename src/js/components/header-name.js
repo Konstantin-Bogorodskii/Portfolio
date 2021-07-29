a3ded438787f512cda9a31f61c57c015220548c4
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-07-29 21:31:14 +0300

    started nav-burger and fixed some bags

--- src/js/components/header-name.js
+++ src/js/components/header-name.js
@@ -1,33 +1,39 @@
 "use strict";
 
 const heroContainer = document.querySelector(".hero__container");
+
 heroContainer.addEventListener("click", (event) => {
   const target = event.target;
   const strText = target.textContent;
   const strSplit = strText.split("");
-  if (target.classList.contains("fancy") || target == strText) {
+
+  let char = 0;
+  let timer = setInterval(onTick, 50);
+
+  function complete() {
+    clearInterval(timer);
+    timer = null;
+  }
+
+  function onTick() {
+    const span = target.querySelectorAll("span")[char];
+    span.classList.add("fancy--active");
+
+    char++;
+    if (char === strSplit.length) {
+      complete();
+      return;
+    }
+  }
+
+  if (target.classList.contains("fancy")) {
     target.textContent = "";
-    console.log(target);
     strSplit.forEach((item) => {
-      target.innerHTML += "<span>" + item + "</span>";
-    });
-    let char = 0;
-    let timer = setInterval(onTick, 50);
-
-    function onTick() {
-      const span = target.querySelectorAll("span")[char];
-      span.classList.add("fancy--active");
-
-      char++;
-      if (char === strSplit.length) {
-        complete();
-        return;
+      if (item === " ") {
+        target.innerHTML += "<span>" + "&nbsp;" + "</span>";
+      } else {
+        target.innerHTML += "<span>" + item + "</span>";
       }
-    }
-
-    function complete() {
-      clearInterval(timer);
-      timer = null;
-    }
+    });
   }
 });
