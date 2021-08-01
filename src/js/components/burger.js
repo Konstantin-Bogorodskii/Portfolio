2adc27cf8cba139a2bf7e633b7bb6a696d8db884
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-08-01 16:59:28 +0300

    finished menu burger

--- src/js/components/burger.js
+++ src/js/components/burger.js
@@ -1,5 +1,7 @@
 const burger = document.querySelector(".burger");
+const navbar = document.querySelector(".nav__list");
 
 burger.addEventListener("click", (e) => {
   e.currentTarget.classList.toggle("burger--active");
+  navbar.classList.toggle("nav__list--active");
 });
--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,38 +1,38 @@
-// Hero
-const heroContainer = document.querySelector(".hero__container");
+// // Hero
+// const heroContainer = document.querySelector(".hero__container");
 
-heroContainer.addEventListener("click", (event) => {
-  const target = event.target;
-  const strText = target.textContent;
-  const strSplit = strText.split("");
+// heroContainer.addEventListener("click", (event) => {
+//   const target = event.target;
+//   const strText = target.textContent;
+//   const strSplit = strText.split("");
 
-  let char = 0;
-  let timer = setInterval(onTick, 50);
+//   let char = 0;
+//   let timer = setInterval(onTick, 50);
 
-  function complete() {
-    clearInterval(timer);
-    timer = null;
-  }
+//   function complete() {
+//     clearInterval(timer);
+//     timer = null;
+//   }
 
-  function onTick() {
-    const span = target.querySelectorAll("span")[char];
-    span.classList.add("fancy--active");
+//   function onTick() {
+//     const span = target.querySelectorAll("span")[char];
+//     span.classList.add("fancy--active");
 
-    char++;
-    if (char === strSplit.length) {
-      complete();
-      return;
-    }
-  }
+//     char++;
+//     if (char === strSplit.length) {
+//       complete();
+//       return;
+//     }
+//   }
 
-  if (target.classList.contains("fancy")) {
-    target.textContent = "";
-    strSplit.forEach((item) => {
-      if (item === " ") {
-        target.innerHTML += "<span>" + "&nbsp;" + "</span>";
-      } else {
-        target.innerHTML += "<span>" + item + "</span>";
-      }
-    });
-  }
-});
+//   if (target.classList.contains("fancy")) {
+//     target.textContent = "";
+//     strSplit.forEach((item) => {
+//       if (item === " ") {
+//         target.innerHTML += "<span>" + "&nbsp;" + "</span>";
+//       } else {
+//         target.innerHTML += "<span>" + item + "</span>";
+//       }
+//     });
+//   }
+// });
