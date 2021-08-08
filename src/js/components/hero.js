ba379d574f7b0025ed0a3b8d9ea4324d60de1ee6
kbogorodsky <konstantin.bogorodsky@gmail.com>
2021-08-08 13:45:02 +0300

    added nav-link + burger animation / fixed hero-animation bugs

--- src/js/components/hero.js
+++ src/js/components/hero.js
@@ -1,38 +1,48 @@
-// // Hero
-// const heroContainer = document.querySelector(".hero__container");
+// Hero
+const heroContainer = document.querySelector(".hero__container");
 
-// heroContainer.addEventListener("click", (event) => {
-//   const target = event.target;
-//   const strText = target.textContent;
-//   const strSplit = strText.split("");
+heroContainer.addEventListener("click", (event) => {
+  const target = event.target;
+  const strText = target.textContent;
+  const strSplit = strText.split("");
 
-//   let char = 0;
-//   let timer = setInterval(onTick, 50);
+  let char = 0;
+  let timer = setInterval(onTick, 50);
+  let isComplete = false;
 
-//   function complete() {
-//     clearInterval(timer);
-//     timer = null;
-//   }
+  function complete() {
+    clearInterval(timer);
+    timer = null;
+  }
 
-//   function onTick() {
-//     const span = target.querySelectorAll("span")[char];
-//     span.classList.add("fancy--active");
+  function onTick() {
+    if (
+      target.classList.contains("container") ||
+      target.classList.contains("btn") ||
+      target.innerHTML == strText
+    )
+      return;
 
-//     char++;
-//     if (char === strSplit.length) {
-//       complete();
-//       return;
-//     }
-//   }
+    const span = target.querySelectorAll("span")[char];
+    span.classList.add("fancy--active");
+    char++;
+    if (char === strSplit.length) {
+      complete();
+      target.innerHTML = strText;
+      return target;
+    }
+  }
 
-//   if (target.classList.contains("fancy")) {
-//     target.textContent = "";
-//     strSplit.forEach((item) => {
-//       if (item === " ") {
-//         target.innerHTML += "<span>" + "&nbsp;" + "</span>";
-//       } else {
-//         target.innerHTML += "<span>" + item + "</span>";
-//       }
-//     });
-//   }
-// });
+  if (target.classList.contains("fancy")) {
+    if (!target.innerHTML == strText) return;
+    target.textContent = "";
+
+    strSplit.forEach((item) => {
+      if (item === " ") {
+        target.innerHTML += "<span>" + "&nbsp;" + "</span>";
+      } else {
+        target.innerHTML += "<span>" + item + "</span>";
+      }
+    });
+  }
+});
